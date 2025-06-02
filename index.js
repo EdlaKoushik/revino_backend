b8aa640da82b94bb9b912a54efd7ce076f6414a4
EdlaKoushik <koushikedla01@gmail.com>
2025-06-02 18:15:44 +0530

    fix: allow deployed frontend domains in CORS config

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173'],
+  origin: [
+    'http://localhost:5174',
+    'http://localhost:5173',
+    'https://revino-frontend.vercel.app',
+    'https://revino-frontend-7zy9x2ocr-edlakoushiks-projects.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
